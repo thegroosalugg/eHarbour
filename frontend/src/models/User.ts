6a38b8849e71309031f6bc247534c610448b9d35
victor loginov <tkoe88@zoho.com>
2024-09-24 15:54:09 +0200

    and again

--- frontend/src/models/User.ts
+++ frontend/src/models/User.ts
@@ -3,7 +3,7 @@ export default class User {
          _id: string;
        email: string;
     username: string;
-   listings?: Listing[];
+    listings: Listing[];
 
   constructor() {
     this._id      = '';
